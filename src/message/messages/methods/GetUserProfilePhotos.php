6a27152a3e6e5ca16d18a1dfc6b401130ad36a0e
uzdevid <uzdevid@gmail.com>
2023-09-28 18:15:22 +0500

    Добавил новый метод

---
+++ src/message/messages/methods/GetUserProfilePhotos.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace uzdevid\telegram\bot\message\messages\methods;
+
+use uzdevid\telegram\bot\message\messages\Method;
+use uzdevid\telegram\bot\message\messages\MethodInterface;
+
+class GetUserProfilePhotos extends Method implements MethodInterface {
+
+    protected static string $__userId = 'user_id';
+    protected static string $__offset = 'offset';
+    protected static string $__limit = 'limit';
+
+    public function __construct(int $userId, array $attributes = []) {
+        parent::__construct($attributes);
+        $this->addAttribute(self::$__userId, $userId);
+    }
+
+    public function methodName(): string {
+        return "getUserProfilePhotos";
+    }
+
+    public function userId(int $userId): self {
+        $this->addAttribute(self::$__userId, $userId);
+        return $this;
+    }
+
+    public function offset(int $offset): self {
+        $this->addAttribute(self::$__offset, $offset);
+        return $this;
+    }
+
+    public function limit(int $limit): self {
+        $this->addAttribute(self::$__limit, $limit);
+        return $this;
+    }
+}
