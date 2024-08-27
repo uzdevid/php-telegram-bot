a344ff33574d3bb271edc443672515f1ca5ecfe9
uzdevid <uzdevid@gmail.com>
2024-08-28 00:14:45 +0500

    Rename abstract class ChatJoinRequest -> ChatJoinRequestHander

---
+++ src/Handler/Request/ChatJoinRequestHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace UzDevid\Telegram\Bot\Handler\Request;
+
+use UzDevid\Telegram\Bot\Type\ChatJoinRequest;
+use Yiisoft\Hydrator\Hydrator;
+
+abstract class ChatJoinRequestHandler {
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return 'chat_join_request';
+    }
+
+    /**
+     * @param array $payload
+     * @return ChatJoinRequest
+     */
+    public function getType(array $payload): ChatJoinRequest {
+        return (new Hydrator())->create(ChatJoinRequest::class, $payload);
+    }
+
+    /**
+     * @param ChatJoinRequest $update
+     *
+     * @return void
+     */
+    abstract public function handle(ChatJoinRequest $update): void;
+}
