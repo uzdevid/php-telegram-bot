fe977d777e0d0981d57f4e53d4cc8d8d7c481313
uzdevid <uzdevid@gmail.com>
2024-09-25 14:55:12 +0500

    Добавил метод sendInvoice

---
+++ src/Message/Message/Method/SendInvoice.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace UzDevid\Telegram\Bot\Message\Message\Method;
+
+use UzDevid\Telegram\Bot\Message\Message\Method;
+use UzDevid\Telegram\Bot\Message\Message\MethodInterface;
+use UzDevid\Telegram\Bot\Type\LabeledPrice;
+
+class SendInvoice extends Method implements MethodInterface {
+    /**
+     * @return string
+     */
+    public function methodName(): string {
+        return 'sendInvoice';
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function title(string $title): static {
+        $this->addAttribute('title', $title);
+        return $this;
+    }
+
+    /**
+     * @param string $description
+     * @return $this
+     */
+    public function description(string $description): static {
+        $this->addAttribute('description', $description);
+        return $this;
+    }
+
+    /**
+     * @param string $payload
+     * @return $this
+     */
+    public function payload(string $payload): static {
+        $this->addAttribute('payload', $payload);
+        return $this;
+    }
+
+    /**
+     * @param string $providerToken
+     * @return $this
+     */
+    public function providerToken(string $providerToken): static {
+        $this->addAttribute('provider_token', $providerToken);
+        return $this;
+    }
+
+    /**
+     * @param string $currency
+     * @return $this
+     */
+    public function currency(string $currency): static {
+        $this->addAttribute('currency', $currency);
+        return $this;
+    }
+
+    /**
+     * @param LabeledPrice $price
+     * @return $this
+     */
+    public function addPrice(LabeledPrice $price): static {
+        $this->pushAttribute('prices', $price);
+        return $this;
+    }
+}
