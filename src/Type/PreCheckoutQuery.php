c59c7a0ec6a7d5d1ba95ac362e88507d49e7bd8d
uzdevid <uzdevid@gmail.com>
2024-09-26 14:53:56 +0500

    Исправил ошибку название свойств

--- src/Type/PreCheckoutQuery.php
+++ src/Type/PreCheckoutQuery.php
@@ -5,9 +5,9 @@ namespace UzDevid\Telegram\Bot\Type;
 use UzDevid\Telegram\Bot\Core\Type;
 
 class PreCheckoutQuery extends Type {
+    public string $id;
     public User $from;
     public string $currency;
-    public string $id;
-    public string $invoice_payload;
-    public int $total_amount;
+    public string $invoicePayload;
+    public int $totalAmount;
 }
