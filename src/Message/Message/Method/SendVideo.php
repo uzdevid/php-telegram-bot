a4d82819f661d2d7e9f21a7559b3d14d0deafc56
uzdevid <uzdevid@gmail.com>
2024-11-01 02:40:08 +0500

    Добавил parseMode для метода SendVideo

--- src/Message/Message/Method/SendVideo.php
+++ src/Message/Message/Method/SendVideo.php
@@ -72,6 +72,16 @@ class SendVideo extends Method implements MethodInterface {
     }
 
     /**
+     * @param string $mode
+     *
+     * @return $this
+     */
+    public function parseMode(string $mode): static {
+        $this->addAttribute('parse_mode', $mode);
+        return $this;
+    }
+
+    /**
      * @param bool $hasSpoiler
      *
      * @return $this
