1ad5fc610703957cda6badb49ed17b5ab74123af
Ibragimov Diko <uzdevid@gmail.com>
2025-12-25 12:31:27 +0500

    Added message handler interface

---
+++ src/handler/MessageHandlerInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace UzDevid\Telegram\Bot\Handler;
+
+use UzDevid\Telegram\Bot\Update\MessageUpdate;
+
+interface MessageHandlerInterface {
+    /**
+     * @param MessageUpdate $update
+     *
+     * @return bool
+     */
+    public static function canHandle(MessageUpdate $update): bool;
+
+    /**
+     * @param MessageUpdate $update
+     *
+     * @return void
+     */
+    public function handle(MessageUpdate $update): void;
+}
